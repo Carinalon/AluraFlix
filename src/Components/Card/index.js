8dc2106b8c2fcb9ffce1e8e7bef1e5917c226c57
Carinalon <alocarin@gmail.com>
2024-07-16 23:35:10 -0300

    actualizacion

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,12 +1,12 @@
 import styles from "./Card.module.css";
 import { Link } from "react-router-dom";
 
-function Card({ id, capa, titulo }) {
+function Card({ id, titulo, categoria, foto, descripcion }) {
   
   return (
     <div className={styles.container}>
-      <Link className={styles.link} to={`/${id}`}>
-        <img src={capa} alt={titulo} className={styles.capa} />
+      <Link className={styles.categoria} to={`/${id}`}>
+        <img src={foto} alt={titulo} className={styles.foto} />
         <h2>{titulo}</h2>
       </Link>
       
